c00a88e6f36bc5d9dad8b9764696fced8329af4b
regul <regul@mantis>
2020-02-04 22:01:20 +0000

    Add solutions for problem 37, 57, 68

---
+++ solutions.clj
@@ -0,0 +1,43 @@
+
+; Solutions for 4clojure problems from Living Clojure book
+; http://www.4clojure.com/
+
+; https://github.com/tantaman/4clojure-solutions
+; Macro for running problems & solutions against their test cases.
+; @author Tantaman
+(defmacro problem [probArgs probBody & solList]
+  `(assert (= 0 (count (filter false? (map
+                                       (fn ~probArgs (and true ~@probBody)) (list ~@solList)))))))
+
+(def _)
+
+; http://www.4clojure.com/problem/37
+; Regex patterns are supported with a special reader macro.
+(problem [_]
+         (list
+          (= _ (apply str (re-seq #"[A-Z]+" "bA1B3Ce "))))
+         "ABC")
+
+; http://www.4clojure.com/problem/57
+; A recursive function is a function which calls itself.
+; This is one of the fundamental techniques used in functional programming.
+(problem [_]
+         (list
+          (= _ ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5)))
+         '(5 4 3 2 1))
+
+; http://www.4clojure.com/problem/68
+; Clojure only has one non-stack-consuming looping construct: recur. Either a function
+; or a loop can be used as the recursion point. Either way, recur rebinds the bindings
+; of the recursion point to the values it is passed.
+; Recur must be called from the tail-position, and calling it elsewhere will result in
+; an error.
+(problem [_]
+         (list
+          (= _
+             (loop [x 5
+                    result []]
+               (if (> x 0)
+                 (recur (dec x) (conj result (+ 2 x)))
+                 result))))
+         [7 6 5 4 3])
