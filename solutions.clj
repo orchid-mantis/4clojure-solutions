072185a5c9fe5bb2c34357fbe3ef55d4a2d72dad
regul <regul@mantis>
2020-02-05 12:58:33 +0000

    Add solution to p71

--- solutions.clj
+++ solutions.clj
@@ -41,3 +41,21 @@
                  (recur (dec x) (conj result (+ 2 x)))
                  result))))
          [7 6 5 4 3])
+
+; http://www.4clojure.com/problem/71
+; The -> macro threads an expression x through a variable number of forms.
+; First, x is inserted as the second item in the first form, making a list of it
+; if it is not a list already. Then the first form is inserted as the second item
+; in the second form, making a list of that form if necessary.
+; This process continues for all the forms. Using -> can sometimes make your code more
+; readable.
+(problem [_]
+         (list
+          (= (_ (sort (rest (reverse [2 5 4 1 3 6]))))
+             (-> [2 5 4 1 3 6]
+                 (reverse)
+                 (rest)
+                 (sort)
+                 (_))
+             5))
+         last)
