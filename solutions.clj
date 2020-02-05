101f9baf82c4357e1421db739c5787a7d0b650cb
regul <regul@mantis>
2020-02-05 14:08:13 +0000

    Add solution to p145

--- solutions.clj
+++ solutions.clj
@@ -68,3 +68,22 @@
              (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (_))
              11))
          #(reduce + %)) ; macro can't handle solution "reduce +"
+
+; http://www.4clojure.com/problem/145
+; Clojure's for macro is a tremendously versatile mechanism for producing a sequence
+; based on some other sequence (s). It can take some time to understand how to use it
+; properly, but that investment will be paid back with clear, concise sequence-wrangling
+; later. With that in mind, read over these for expressions and try to see how
+; each of them produces the same result.
+(problem [_]
+         (list
+          (= _ (for [x (range 40)
+                      :when (= 1 (rem x 4))]
+                  x))
+          (= _ (for [x (iterate #(+ 4 %) 0)
+                      :let [z (inc x)]
+                      :while (< z 40)]
+                  z))
+          (= _ (for [[x y] (partition 2 (range 20))]
+                  (+ x y))))
+         '(1 5 9 13 17 21 25 29 33 37))
