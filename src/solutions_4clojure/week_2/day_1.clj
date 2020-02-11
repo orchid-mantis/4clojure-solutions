701e6d1c9068055bcaf944dbc3539db61fda754d
regul <regul@mantis>
2020-02-11 16:53:44 +0000

    Add solution to p48

--- src/solutions_4clojure/week_2/day_1.clj
+++ src/solutions_4clojure/week_2/day_1.clj
@@ -41,3 +41,13 @@
                          (and (>= intCh (int \A)) (<= intCh (int \Z))))]
             (apply str (filter (fn [ch]
                                  (when (upper? (int ch)) ch)) input))))
+
+; http://www.4clojure.com/problem/48
+; The some function takes a predicate function and a collection.
+; It returns the first logical true value of (predicate x) where x is an item
+; in the collection.
+(problem [_]
+         (list
+          (= _ (some #{2 7 6} [5 6 7 8]))
+          (= _ (some #(when (even? %) %) [5 6 7 8])))
+         6)
