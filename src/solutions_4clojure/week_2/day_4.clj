c07a1d9c52460f80ff8ddaf200169634785f97f6
regul <regul@mantis>
2020-02-14 18:52:36 +0000

    Add solution to p100

--- src/solutions_4clojure/week_2/day_4.clj
+++ src/solutions_4clojure/week_2/day_4.clj
@@ -20,3 +20,24 @@
          (fn [s1 s2] (let [unioned (clojure.set/union s1 s2)
                            intersected (clojure.set/intersection s1 s2)]
                      (clojure.set/difference unioned intersected))))
+
+; http://www.4clojure.com/problem/100
+; Write a function which calculates the least common multiple.
+; Your function should accept a variable number of positive integers or ratios.
+; lcm(a, b) = a*b / gcd(a, b)
+(problem [_]
+         (list
+          (== (_ 2 3) 6)
+          (== (_ 5 3 7) 105)
+          (== (_ 1/3 2/5) 2)
+          (== (_ 3/4 1/6) 3/2)
+          (== (_ 7 5/7 2 3/5) 210)
+          )
+         (fn [& numbers]
+           (let [gcd (fn [x y] (loop [u x
+                                      w y]
+                                 (if (= w 0)
+                                   u
+                                   (recur w (mod u w)))))
+                 lcm (fn [x y] (/ (* x y) (gcd x y)))]
+             (reduce lcm numbers))))
