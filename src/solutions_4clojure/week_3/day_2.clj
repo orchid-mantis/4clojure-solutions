495349501ef1a078dfaa3c4706bc63626d8482bd
regul <regul@mantis>
2020-02-16 23:08:46 +0000

    Add solution to p44

--- src/solutions_4clojure/week_3/day_2.clj
+++ src/solutions_4clojure/week_3/day_2.clj
@@ -17,3 +17,20 @@
          (fn [f] 
            (fn [a b]
              (f b a))))
+
+; http://www.4clojure.com/problem/44
+; Write a function which can rotate a sequence in either direction.
+(problem [_]
+         (list
+          (= (_ 2 [1 2 3 4 5]) '(3 4 5 1 2))
+          (= (_ -2 [1 2 3 4 5]) '(4 5 1 2 3))
+          (= (_ 6 [1 2 3 4 5]) '(2 3 4 5 1))
+          (= (_ 1 '(:a :b :c)) '(:b :c :a))
+          (= (_ -4 '(:a :b :c)) '(:c :a :b)))
+         (fn rotate [n coll]
+           (let [size (count coll)
+                 n (if (neg? n)
+                     (* (- n) (- size 1))
+                     n)
+                 mod-n (mod n size)]
+             (drop mod-n (take (+ mod-n size) (cycle coll))))))
