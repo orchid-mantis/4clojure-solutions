ed3f152603d90b6df34f9513aa105e143ae34c83
regul <regul@mantis>
2020-02-17 20:21:22 +0000

    Add solution to p50

--- src/solutions_4clojure/week_3/day_3.clj
+++ src/solutions_4clojure/week_3/day_3.clj
@@ -15,3 +15,17 @@
           (= (_ (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9))))
          (fn reverse-interleave [coll n]
            (apply map list (partition n coll))))
+
+; http://www.4clojure.com/problem/50
+; Write a function which takes a sequence consisting of items with different
+; types and splits them up into a set of homogeneous sub-sequences.
+; The internal order of each sub-sequence should be maintained,
+; but the sub-sequences themselves can be returned in any order
+; (this is why 'set' is used in the test cases).
+(problem [_]
+         (list
+          (= (set (_ [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]})
+          (= (set (_ [:a "foo"  "bar" :b])) #{[:a :b] ["foo" "bar"]})
+          (= (set (_ [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]}))
+         (fn split-by-type [coll]
+           (map second (group-by type coll))))
